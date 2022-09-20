405a689bbb32b1a524c8323f46d5f0272777cdb5
joyalfrancis23 <joyalfrancis23@gmail.com>
2022-09-20 18:20:50 +0530

    Update

--- lib/pages/register_type.dart
+++ lib/pages/register_type.dart
@@ -11,40 +11,50 @@ class RegisterType extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children:[
-          const Center(child: Text('St Sebastian\'s Church, Chellanam\nRegister',textAlign: TextAlign.center,style: TextStyle(fontSize: 18),)),
-          Row(
-            children: [
-              InkWell(
-              child: const SizedBox(
-                height: 100,
-                width: 100,
-                child: Text('Birth Register'),
-              ),
-              onTap: (){
-                
-              },
-              ),
-              InkWell(
-              child: const SizedBox(
-                height: 100,
-                width: 100,
-                child: Text('Marriage Register'),
-              ),
-              onTap: (){
-                
-              },
-              ),
-              InkWell(
-              child: const SizedBox(
-                height: 100,
-                width: 100,
-                child: Text('Death Register'),
-              ),
-              onTap: (){
-                
-              },
-              ),
-            ]
+          const Center(child: Text('St Sebastian\'s Church, Chellanam\nRegister\n\n\n\n',textAlign: TextAlign.center,style: TextStyle(fontSize: 18),)),
+          Padding(
+            padding: const EdgeInsets.all(17),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                  child: const Text('Birth Register',style: TextStyle(),textAlign: TextAlign.center,),
+                ),
+                onTap: (){
+                  
+                },
+                ),
+                InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(18),
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                  child: const Text('Marriage Register',style: TextStyle(),textAlign: TextAlign.center,),
+                ),
+                onTap: (){
+                  
+                },
+                ),
+                InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                  child: const Text('Death Register',style: TextStyle(),textAlign: TextAlign.center,),
+                ),
+                onTap: (){
+                  
+                },
+                ),
+              ]
+            ),
           ),
           ],
 
